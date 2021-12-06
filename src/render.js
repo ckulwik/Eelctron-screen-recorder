1f6cdb74ba0bd771bc1ef0c2a9ed9ead6bc3b700
Chris Kulwik <ckulwik@umich.edu>
2021-12-05 21:47:42 -0500

    adding capure and save functionality

--- src/render.js
+++ src/render.js
@@ -12,8 +12,19 @@ const recordedChunks = [];
 const videoElement = document.querySelector("video");
 
 const startBtn = document.getElementById("startBtn");
+startBtn.onclick = (e) => {
+  mediaRecorder.start();
+  startBtn.classList.add("is-danger");
+  startBtn.innerText = "Recording";
+};
 
 const stopBtn = document.getElementById("stopBtn");
+
+stopBtn.onclick = (e) => {
+  mediaRecorder.stop();
+  startBtn.classList.remove("is-danger");
+  startBtn.innerText = "Start";
+};
 
 const videoSelectBtn = document.getElementById("videoSelectBtn");
 videoSelectBtn.onclick = getVideoSources;
@@ -56,4 +67,38 @@ const selectSource = async (source) => {
   // Preview the source in a video element
   videoElement.srcObject = stream;
   videoElement.play();
+
+  // Create the Media Recorder
+  const options = { mimeType: "video/webm; codecs=vp9" };
+  mediaRecorder = new MediaRecorder(stream, options);
+
+  // Register Event Handlers
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop;
+
+  // Updates the UI
+};
+
+// Captures all recorded chunks
+const handleDataAvailable = (e) => {
+  console.log("video data available");
+  recordedChunks.push(e.data);
+};
+
+// Saves the video file on stop
+const handleStop = async (e) => {
+  const blob = new Blob(recordedChunks, {
+    type: "video/webm; codecs=vp9",
+  });
+
+  const buffer = Buffer.from(await blob.arrayBuffer());
+
+  const { filePath } = await dialog.showSaveDialog({
+    buttonLabel: "Save video",
+    defaultPath: `vid-${Date.now()}.webm`,
+  });
+
+  if (filePath) {
+    writeFile(filePath, buffer, () => console.log("video saved successfully!"));
+  }
 };
